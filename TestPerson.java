ee4b26f9611f8ac80e2e7bb6d1f270247c55b758
จิราภา ทรงโฉม < >
2017-02-16 13:54:58 +0700

    ลบคอมเม้น

--- TestPerson.java
+++ TestPerson.java
@@ -7,10 +7,10 @@ package INT105.Person;
  */
 public class TestPerson {
     public static void main(String[] args){
-        /*Person p1 = new Person(123456789,"Jirapa","Songchom");
-        System.out.println(p1);*/
-        // em = new Student("59130500012", 1149900502, "Jirapa", "Songchom");
+        Person p1 = new Person(123456789,"Jirapa","Songchom");
+        System.out.println(p1);
+        em = new Student("59130500012", 1149900502, "Jirapa", "Songchom");
        
-        //System.out.println(em.stdIdChecking("59130500012","Jirapa"));
+        System.out.println(em.stdIdChecking("59130500012","Jirapa"));
     }
 }
